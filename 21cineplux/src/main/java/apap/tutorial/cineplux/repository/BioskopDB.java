b5517560c309024735cff249b6417f5d4640c867
“Andreas <andreasayuilham@gmail.com>
2021-11-24 12:18:50 +0700

    demo tutorial 6

--- 21cineplux/src/main/java/apap/tutorial/cineplux/repository/BioskopDB.java
+++ 21cineplux/src/main/java/apap/tutorial/cineplux/repository/BioskopDB.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 @Repository
 public interface BioskopDB extends JpaRepository<BioskopModel, Long> {
     Optional<BioskopModel> findByNoBioskop(Long noBioskop);
-    List<BioskopModel> findAllByOrderByNamaBioskop();
+//    List<BioskopModel> findAllByOrderByNamaBioskop();
 }
